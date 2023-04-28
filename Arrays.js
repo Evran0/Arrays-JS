d9474f2db479b05f500b41ced040ef0f6b770902
Evran0 <eddypolisabuzi59@gmail.com>
2023-04-28 17:38:10 +0200

    first commit

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+
+const numbers = [1, 2, 3, 4, 5];
+const lastnumber = numbers.pop();
+console.log(lastnumber);
+
+
+const firstNumber = numbers.shift();
+console.log(firstNumber);
+
+numbers.push(4, 5);
+console.log(numbers);
+
+const includesThree = numbers.includes(3);
+console.log(includesThree);
+
